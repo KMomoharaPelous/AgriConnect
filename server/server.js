60bef6c7def1fcf42cbbcb1a3aef9274e4b75e08
Kichelle Momohara-Pelous <87834614+KMomoharaPelous@users.noreply.github.com>
2025-09-03 07:13:43 -1000

    Updated Server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI)
     });
 
 // Routes
-app.get('/', (req, res) => {
+app.get('/api/health', (req, res) => {
     res.json({ message: '🌿 AgriConnect API is running!' });
 });
 
